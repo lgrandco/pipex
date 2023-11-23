c3e988f42c5aefb21bed8e8e38b662e3c55e4a51
Leo Grandcoin <l.grandcoinlaf@gmail.com>
2023-11-23 18:18:23 +0100

    z

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:19:32 by legrandc          #+#    #+#             */
-/*   Updated: 2023/11/22 19:14:17 by legrandc         ###   ########.fr       */
+/*   Updated: 2023/11/23 18:12:08 by legrandc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,23 @@ char	*get_path(char *program, char **env)
 		free(path_tried);
 		paths++;
 	}
-	return 0;
+	return (0);
+}
+
+void	write_heredoc(char *end, int fd)
+{
+	char	*line;
+
+	while (1)
+	{
+		ft_printf("> ");
+		line = get_next_line(0);
+		if (!ft_strncmp(line, end, ft_strlen(line) - 1))
+			break ;
+		ft_putstr_fd(line, fd);
+		free(line);
+	}
+	free(line);
 }
 
 void	exec_pipe(char *command, char **ev, int is_last, int fd_outfile)
@@ -71,14 +87,29 @@ int	main(int ac, char **av, char **ev)
 	int	fd_infile;
 	int	fd_outfile;
 
-	fd_infile = open(av[1], O_RDONLY);
-	fd_outfile = open(av++[ac-1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if (ac < 5)
+		exit(EXIT_FAILURE);
+	if (!strcmp(av[1], "here_doc"))
+	{
+		fd_infile = open("/tmp/here_doc", O_TRUNC | O_RDWR | O_CREAT, 0644);
+		write_heredoc(av[2], fd_infile);
+		fd_infile = open("/tmp/here_doc", O_WRONLY, 0644);
+		fd_outfile = open(av[ac - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		av++;
+	}
+	else
+	{
+		fd_infile = open(av[1], O_RDONLY | O_CREAT, 066);
+		fd_outfile = open(av[ac - 1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	}
+	av++;
 	if (fd_infile == -1 || fd_outfile == -1)
 		return (1);
 	dup2(fd_infile, STDIN_FILENO);
 	close(fd_infile);
 	while (*(++av + 1))
 	{
+		dprintf(2, "[%s]\n\n\n", *av);
 		exec_pipe(*av, ev, *(av + 2) == NULL, fd_outfile);
 	}
 	close(fd_outfile);
